b3ee3df49b8abbad23e8e4cf6aca99e94eeffc9e
VladSurzhko <surzhkovlad69@gmail.com>
2023-04-12 13:26:12 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+
+ const btnStart = document.querySelector("[data-start]")
+ const btnStop = document.querySelector("[data-stop]")
+ let timerId = null
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  
+
+  btnStart.addEventListener("click", () => {
+    timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;    
+    }, 1000);
+    })
+
+    btnStop.addEventListener("click", () => {
+        clearInterval(timerId);
+      });
+
+
+
+    
